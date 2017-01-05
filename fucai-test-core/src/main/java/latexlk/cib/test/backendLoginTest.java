f90f83068016de541f4992f77d6b869277b7ea2a
Surface\RubberLK <jy0001087@163.com>
2017-01-05 20:26:53 +0800

    同步

---
+++ fucai-test-core/src/main/java/latexlk/cib/test/backendLoginTest.java
@@ -0,0 +1,13 @@
+package latexlk.cib.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+/**
+ * Created by Bud on 2017/1/5.
+ */
+public class backendLoginTest {
+    WebDriver driver = new ChromeDriver();
+    driver.
+}
