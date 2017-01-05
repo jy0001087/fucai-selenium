b7adf6a28257752f717b7e425eaa11dff022700e
Base-PC <jy0001087@163.com>
2017-01-05 23:18:52 +0800

    调起chrome成功

--- fucai-test-core/src/main/java/latexlk/cib/test/backendLoginTest.java
+++ fucai-test-core/src/main/java/latexlk/cib/test/backendLoginTest.java
@@ -2,12 +2,34 @@ package latexlk.cib.test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.io.File;
 
 
 /**
  * Created by Bud on 2017/1/5.
  */
 public class backendLoginTest {
-    WebDriver driver = new ChromeDriver();
-    driver.
+
+    private static ChromeDriverService service;
+
+    public static void main(String[] args) {
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("chromedriver.exe"))
+                .usingAnyFreePort()
+                .build();
+        try {
+            service.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        WebDriver dr = new RemoteWebDriver(service.getUrl(),
+                         DesiredCapabilities.chrome());
+        String url = "http://www.baidu.com";
+        dr.get(url);
+        dr.quit();
+    }
 }
