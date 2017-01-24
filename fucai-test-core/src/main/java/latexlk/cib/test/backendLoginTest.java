4763cea3da94f89b21849ccbba8264a17858a7c4
Surface\RubberLK <jy0001087@163.com>
2017-01-24 17:23:19 +0800

    微信页面测试

--- fucai-test-core/src/main/java/latexlk/cib/test/backendLoginTest.java
+++ fucai-test-core/src/main/java/latexlk/cib/test/backendLoginTest.java
@@ -22,10 +22,11 @@ public class backendLoginTest {
     public static void main(String[] args) {
         backendLoginTest bt = new backendLoginTest();
         String url_web = "http://www.cigage.com:9999/xnlottery/resources/backendLogin.jsp";
-        String url_wechat = "http://http://wellot.wind4us.com:8080/xnlottery/resources/wechat/bind/bind.jsp";
+        String url_wechat = "http://wellot.wind4us.com:8080/xnlottery/resources/wechat/bind/bind.jsp";
 //          for (int i = 0; i < 10; i++) {
             WebDriver dr = bt.getDriver();
             dr.get(url_wechat);
+        bt.wechat_jspTest(dr);
 //            dr.get(url_web);
 //            bt.jspTest(dr);
   //      }
@@ -33,7 +34,25 @@ public class backendLoginTest {
 
     public  void wechat_jspTest(WebDriver dr){
         WebElement name = dr.findElement(By.name("signContractName"));
+        WebElement phoneNum = dr.findElement(By.name("phoneNum"));
+        WebElement IdCard = dr.findElement(By.name("identityCardNum"));
+        WebElement bankCard = dr.findElement(By.name("bankCardNum"));
+        WebElement bittingShop = dr.findElement(By.name("bettingShopNum"));
+        WebElement authCodeButton = dr.findElement(By.xpath("//*[@id=\"bind_form\"]/div[2]/div[6]/div[3]/button"));
+
         name.sendKeys("李科");
+        phoneNum.sendKeys("18697265816");
+        IdCard.sendKeys("63010519860625201X");
+        bankCard.sendKeys("622908633005844511");
+        bittingShop.sendKeys("63010006");
+        authCodeButton.click();
+
+        String sttringSMS = JOptionPane.showInputDialog("收到的短信验证码");
+        WebElement inputSMS = dr.findElement(By.name("checkCode"));
+        inputSMS.sendKeys(sttringSMS);
+
+        WebElement submitButton = dr.findElement(By.id("showTooltips"));
+        submitButton.click();
     }
     public WebDriver getDriver() {
         service = new ChromeDriverService.Builder()
